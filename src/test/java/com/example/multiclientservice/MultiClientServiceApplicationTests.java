93297307a6f48bd65d4cc26455377cd974a713fa
pleasebeavailable <ante_sucur@hotmail.com>
2020-02-20 13:58:02 +0100

    Implement jwt security

---
+++ src/test/java/com/example/multiclientservice/MultiClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multiclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiClientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
